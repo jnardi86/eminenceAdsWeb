b2fa7b50ab357d5c8cbc85910f7c3f6bdfcbe2d6
jnardi86 <julian.nardi@gmail.com>
2025-01-14 22:55:51 -0300

    add new whatsapp number

--- src/components/WhatsAppIcon.jsx
+++ src/components/WhatsAppIcon.jsx
@@ -3,10 +3,10 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 const WhatsAppIcon = () => {
   return (
-    <a 
-      href="https://wa.me/+5491136455591"
+    <a
+      href="https://wa.me/+5491169687423"
       className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-transform transform hover:scale-105 duration-1000 z-50"
-      target="_blank" 
+      target="_blank"
       rel="noopener noreferrer"
     >
       <FaWhatsapp className="text-3xl" />
